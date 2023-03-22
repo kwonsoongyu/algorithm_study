8521e4e9363329981ccdd9c82ad04a7597303d2c
kwonsoongyu <49389225+kwonsoongyu@users.noreply.github.com>
2023-03-22 17:26:13 +0900

    [BOJ] 11726번 2xn 타일링 / Silver 3

---
+++ BOJ_SoonGyu/11726.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+N = int(input())
+graph = [0 for i in range(1001)]
+
+graph[1] = 1
+graph[2] = 2
+
+for i in range(3,1001):
+    graph[i] = (graph[i-1] + graph[i-2])%10007
+
+print(graph[N])
